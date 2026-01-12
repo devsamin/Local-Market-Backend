034bf1bc56059e5a58d303807a8e355a6aab280e
devsamin <abdullahalsamin79@gmail.com>
2026-01-12 09:33:09 -0800

    add cloudinary

--- specialOffer/models.py
+++ specialOffer/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 class SpecialOffer(models.Model):
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
-    image = models.FileField(upload_to="special_offers/" , blank=True, null=True)
+    # image = models.FileField(upload_to="special_offers/" , blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True)
     badge = models.CharField(max_length=100)
     badgeColor = models.CharField(max_length=50, default="bg-green-500")
     created_at = models.DateTimeField(auto_now_add=True)
--- specialOffer/serializers.py
+++ specialOffer/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import SpecialOffer
 
 class SpecialOfferSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
     class Meta:
         model = SpecialOffer
         fields = "__all__"
+    def get_image(self, obj):
+        return obj.image.url if obj.image else None
