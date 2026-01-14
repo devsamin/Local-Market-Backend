2cd07067e1aa8cfeb4131da8d9c7005c485baf47
devsamin <abdullahalsamin79@gmail.com>
2026-01-14 06:54:11 -0800

    add product modifi for cloudinary

--- products/models.py
+++ products/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from category.models import Category
-
+from cloudinary.models import CloudinaryField
 
 class Product(models.Model):
     seller = models.ForeignKey(
@@ -14,9 +14,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.PositiveIntegerField(default=0)  # Discount in percentage
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to="products/", blank=True, null=True)
-    image2 = models.ImageField(upload_to="products/", blank=True, null=True)
-    image3 = models.ImageField(upload_to="products/", blank=True, null=True)
+    # image = models.ImageField(upload_to="products/", blank=True, null=True)
+    # image2 = models.ImageField(upload_to="products/", blank=True, null=True)
+    # image3 = models.ImageField(upload_to="products/", blank=True, null=True)
+    image = CloudinaryField("image", blank=True, null=True)
+    image2 = CloudinaryField("image2", blank=True, null=True)
+    image3 = CloudinaryField("image3", blank=True, null=True)
     categories = models.ManyToManyField(Category, related_name="products", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
